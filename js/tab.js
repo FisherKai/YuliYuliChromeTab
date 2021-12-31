1a77d13a6ce15635dcb72f4b9556ef7054632920
余凯 <yukai@egova.com.cn>
2021-12-31 13:32:43 +0800

    添加容器

--- js/tab.js
+++ js/tab.js
@@ -1,7 +1,8 @@
-window.onload = function () {
-    loadLive2d();
-    addSearchEvent();
-}
+// 定义全局变量
+var SEO = "baidu"; // 定义搜索引擎
+var _container_left = document.getElementById('left');
+var _container_center = document.getElementById('center');
+var _container_right = document.getElementById('right');
 
 /**
  * 加载看板娘
@@ -17,11 +18,60 @@ function addSearchEvent() {
     var searchEle = document.getElementById("search-input");
     searchEle.onkeydown = function (e) {
         if (e.keyCode === 13) {
-            window.open(`https://www.baidu.com/s?ie=UTF-8&wd=${e.target.value}`, "_parent");
+            var baseUrl = SEO === "baidu" ? `https://www.baidu.com/s?ie=UTF-8&wd=` : `https://www.google.com/search?q=`;
+            window.open(`${baseUrl}${e.target.value}`, "_parent");
         }
     }
 
     // 给search-icon添加icon选择
     var searchIcon = document.getElementById("search-icon");
-    searchIcon.setAttribute("src","./images/icon/baidu.png")
+    searchIcon.setAttribute("src", "./images/icon/baidu.png");
+    searchIcon.onclick = function () {
+        console.log(searchIcon.getAttribute("src") === "./images/icon/baidu.png");
+        if (searchIcon.getAttribute("src") === "./images/icon/baidu.png") {
+            searchIcon.setAttribute("src", "./images/icon/google.png ");
+            SEO = "google";
+        } else {
+            searchIcon.setAttribute("src", "./images/icon/baidu.png");
+            SEO = "baidu";
+        }
+    }
+}
+
+// 加载容器左边
+function loadLeftContainer() {
+    console.log(chrome);
+    createItem();
+}
+
+function createItem() {
+    // 创建节点
+    var vDom = document.createDocumentFragment();
+    var item = document.createElement('div');
+    var introduce = document.createElement('p');
+    var time = document.createElement('span');
+    var number = document.createElement('span');
+
+    // 对节点添加class
+    item.setAttribute("class", "container-item");
+    introduce.setAttribute("class", "container-introduce");
+    time.setAttribute("class", "container-time");
+    number.setAttribute("class", "container-number");
+
+    // todo 对节点添加数据
+    introduce.innerHTML = "测试标题";
+    time.innerHTML = "2021.09.12";
+    number.innerHTML = "2980";
+
+    item.appendChild(introduce);
+    item.appendChild(time);
+    item.appendChild(number);
+    vDom.appendChild(item);
+    _container_left.appendChild(vDom);
+}
+
+window.onload = function () {
+    loadLive2d();
+    addSearchEvent();
+    loadLeftContainer();
 }
