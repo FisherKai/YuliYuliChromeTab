d18575401e88c5813eeae9e0e1dec4e769f8e224
yukai pc <1079869931@qq.com>
2021-12-31 09:00:39 +0800

    添加搜索框图标

--- js/tab.js
+++ js/tab.js
@@ -7,15 +7,21 @@ window.onload = function () {
  * 加载看板娘
  */
 function loadLive2d() {
-   initModel();
+    initModel();
 }
 
+// 谷歌搜索 https://www.google.com/search?q=
+// 百度搜索 https://www.baidu.com/s?ie=UTF-8&wd=
 function addSearchEvent() {
+    // 给搜索框添加回车查询事件
     var searchEle = document.getElementById("search-input");
     searchEle.onkeydown = function (e) {
         if (e.keyCode === 13) {
-            console.log(e.target.value);
-            window.open(`https://www.baidu.com/s?ie=UTF-8&wd=${e.target.value}`,"_parent");
+            window.open(`https://www.baidu.com/s?ie=UTF-8&wd=${e.target.value}`, "_parent");
         }
     }
+
+    // 给search-icon添加icon选择
+    var searchIcon = document.getElementById("search-icon");
+    searchIcon.setAttribute("src","./images/icon/baidu.png")
 }
