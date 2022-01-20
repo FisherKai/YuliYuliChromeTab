be5a9a1850633b5463df4d1317b7e2421d1d4d49
yukai pc <1079869931@qq.com>
2022-01-21 00:05:27 +0800

    update

--- js/tab.js
+++ js/tab.js
@@ -8,11 +8,16 @@ function loadLive2d() {
     initModel();
 }
 
+function readHistory() {
+
+}
+
 // 谷歌搜索 https://www.google.com/search?q=
 // 百度搜索 https://www.baidu.com/s?ie=UTF-8&wd=
 function addSearchEvent() {
     // 给搜索框添加回车查询事件
     var searchEle = document.getElementById("search-input");
+    searchEle.focus();
     searchEle.onkeydown = function (e) {
         if (e.keyCode === 13) {
             var baseUrl = SEO === "baidu" ? `https://www.baidu.com/s?ie=UTF-8&wd=` : `https://www.google.com/search?q=`;
@@ -31,6 +36,31 @@ function addSearchEvent() {
             searchIcon.setAttribute("src", "./images/icon/baidu.png");
             SEO = "baidu";
         }
+        setTimeout(function () {
+            var searchEle = document.getElementById("search-input");
+            searchEle.focus();
+        }, 0);
+    }
+}
+
+/**
+ * 全局监听tab事件
+ */
+function listenerKeyDown() {
+    document.body.onkeydown = function (e) {
+        var searchIcon = document.getElementById("search-icon");
+        if (searchIcon.getAttribute("src") === "./images/icon/baidu.png") {
+            searchIcon.setAttribute("src", "./images/icon/google.png ");
+            SEO = "google";
+        } else {
+            searchIcon.setAttribute("src", "./images/icon/baidu.png");
+            SEO = "baidu";
+        }
+        setTimeout(function () {
+            var searchEle = document.getElementById("search-input");
+            searchEle.focus();
+        }, 0);
+
     }
 }
 
@@ -38,4 +68,5 @@ window.onload = function () {
     // 加载第三方插件
     loadLive2d();
     addSearchEvent();
+    listenerKeyDown();
 }
